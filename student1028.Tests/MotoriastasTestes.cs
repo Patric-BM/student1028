03be64f62ac50c6852092e733b36fb3918f8dc28
Patric Borges <patric.costaborges@hoymail.com>
2024-02-23 19:12:29 -0300

    Alguns testes de exemplo

--- student1028.Tests/MotoriastasTestes.cs
+++ student1028.Tests/MotoriastasTestes.cs
@@ -46,26 +46,5 @@ namespace student1028.Tests
             // Assert
             act.Should().Throw<Exception>().WithMessage("A viagem não será realizada devido falta de motoristas!");
         }
-
-        [Theory(DisplayName = "EncontrarMotoristas com mais de duas pessoas habilitadas")]
-        [Trait("Categoria", "Motorista Tests")]
-        [InlineData("João", 20, true, "Maria", 22, true, "Carlos", 23, true)]
-        public void EncontrarMotoristas_MaisDeDuasPessoasHabilitadas_RetornaPrimeirosDoisNomes(string nome1, int idade1, bool habilitacao1, string nome2, int idade2, bool habilitacao2, string nome3, int idade3, bool habilitacao3)
-        {
-            // Arrange
-            var motorista = new Motorista();
-            var pessoas = new List<Pessoa>
-            {
-                new Pessoa { Nome = nome1, Idade = idade1, PossuiHabilitacaoB = habilitacao1 },
-                new Pessoa { Nome = nome2, Idade = idade2, PossuiHabilitacaoB = habilitacao2 },
-                new Pessoa { Nome = nome3, Idade = idade3, PossuiHabilitacaoB = habilitacao3 }
-            };
-
-            // Act
-            var resultado = motorista.EncontrarMotoristas(pessoas);
-
-            // Assert
-            resultado.Should().Be($"{nome1}, {nome2}");
-        }
     }
 }
